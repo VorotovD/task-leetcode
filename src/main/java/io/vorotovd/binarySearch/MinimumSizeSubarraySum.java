de6016b30e7178825a40945c0834a4658651d610
Воротов Даниил <swarog.5611@yandex.ru>
2022-10-05 20:03:14 +0300

    First commit

---
+++ src/main/java/io/vorotovd/binarySearch/MinimumSizeSubarraySum.java
@@ -0,0 +1,17 @@
+package io.vorotovd.binarySearch;
+
+public class MinimumSizeSubarraySum {
+    public static void main(String[] args) {
+        MinimumSizeSubarraySumSolution solution = new MinimumSizeSubarraySumSolution();
+        int[] nums = {2,3,1,2,4,3};
+        int target = 7;
+        System.out.println(solution.minSubArrayLen(target,nums));
+    }
+}
+
+class  MinimumSizeSubarraySumSolution {
+
+    public int minSubArrayLen(int target, int[] nums) {
+
+    }
+}
