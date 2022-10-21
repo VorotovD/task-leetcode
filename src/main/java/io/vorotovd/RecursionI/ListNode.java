f4b6f1926146619f31afa1b16934f51ddc19cc16
Воротов Даниил <swarog.5611@yandex.ru>
2022-10-21 13:10:58 +0300

    Element of LinkedList

---
+++ src/main/java/io/vorotovd/RecursionI/ListNode.java
@@ -0,0 +1,15 @@
+package io.vorotovd.RecursionI;
+
+
+/**
+ * Definition for singly-linked list
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+}
